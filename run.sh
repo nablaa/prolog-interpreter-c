15b767eb89456ebfc37fe5ca330be6365426cdf7
Miika-Petteri Matikainen <miikapetteri@gmail.com>
2011-07-04 23:31:32 +0300

    Make running valgrind a command line argument in run.sh

--- run.sh
+++ run.sh
@@ -1,3 +1,14 @@
-#!/bin/sh
-#colorgcc -Wall -Wextra -pedantic -g -std=c99 -o main *.c && ./main
-colorgcc -Wall -Wextra -pedantic -g -std=c99 -o main *.c && valgrind --tool=memcheck --leak-check=full --show-reachable=yes ./main
+#!/bin/bash
+
+if [ $# -gt 0 ]; then
+	valgrind=$1
+fi
+
+if [ $valgrind == "valgrind" ]; then
+	cmd="valgrind --tool=memcheck --leak-check=full --show-reachable=yes ./main"
+else
+	cmd="./main"
+fi
+
+
+colorgcc -Wall -Wextra -pedantic -g -std=c99 -o main *.c && $cmd
